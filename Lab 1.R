bb1b1d28e47cba45a9cb62765ad2abf29cd5276f
Sumaiya Tasnim <90747318+SumaiyaTasnim000@users.noreply.github.com>
2024-11-12 19:53:57 +0600

    Basic R codes
    basic syntax, loops, flooring, else if, switch in R language.

---
+++ Lab 1.R
@@ -0,0 +1,33 @@
+1+1
+2+3
+5+4
+sqrt(10)
+x <- 1
+y <- 2
+z <- 3
+x * y * z
+# nishat
+ceiling(10.7)
+floor(10.7)
+Q <- "EWW Nishat"
+tolower(Q)
+strsplit(Q, split=" ")
+str <- "Meow Kitty"
+nchar(str)
+paste0(Q, str)
+# If else:
+a <- 50
+b <- 50
+if (a>b){
+  print("a is greater than b")
+} else if (a == b){
+  print("a is same as b")
+} else {
+  print("a is not greater than b")
+}
+# Ifelse:
+score 
+ifelse
+#switch
+switch(1, "red","purple", "white", "blue",)
+
